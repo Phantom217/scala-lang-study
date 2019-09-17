8cd8c0b76f50645c3ff2f2d3ddd73d75f68c8f5c
Tyler Oalman <tyleroalman@gmail.com>
2019-09-16 22:07:27 -0500

    Scala: Add generic class usage

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,34 @@
 import samples._
+import extras._
 
 object Main extends App {
-  TLoop.loops
+  extras.StreamStyle
+  // JavaInput.jinput
+  stack_int
+  stack_string
+
+
+  def stack_int: Unit = {
+    val stack = new GenericClasses.Stack[Int]
+    stack.push(1)
+    stack.push(2)
+    println(stack.pop)  // prints 2
+    println(stack.pop)  // prints 1
+  }
+
+  class Fruit
+  class Apple extends Fruit
+  class Banana extends Fruit
+
+  def stack_string: Unit = {
+    val stack  = new GenericClasses.Stack[Fruit]
+    val apple  = new Apple
+    val banana = new Banana
+
+    stack.push(apple)
+    stack.push(banana)
+
+    println(stack.pop)
+    println(stack.pop)
+  }
 }
