804dd8e75a5a1aec4f0b5aded9201196f51a2ccd
Tyler Oalman <tyleroalman@gmail.com>
2019-09-18 21:31:36 -0500

    "Scala: Add return type to push()"

--- src/main/scala/extras/Generics.scala
+++ src/main/scala/extras/Generics.scala
@@ -5,7 +5,7 @@ object GenericClasses {
 
   class Stack[A] {    // Stack of any type denoted as [A]
     private var elements: List[A] = Nil
-    def push(x: A) { elements = x :: elements }
+    def push(x: A): Unit = { elements = x :: elements }
     def peek: A = elements.head
     def pop(): A = {
       val currentTop = peek
