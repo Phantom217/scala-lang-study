1481c138fa92010d42d4a95fd809c9dc8ef14dca
Tyler Oalman <tyleroalman@gmail.com>
2019-09-20 16:31:02 -0500

    Scala: Change AnonFunc to an object instead of class
    Also removed example code in the file. Moved to the examples directory.

--- src/main/scala/extras/AnonFunc.scala
+++ src/main/scala/extras/AnonFunc.scala
@@ -1,6 +1,6 @@
 package extras
 
-class AnonFunc {
+object AnonFunc {
   // Anonymous Functions
 
   def sum(f: Int => Int, a: Int, b: Int): Int =
@@ -10,11 +10,4 @@ class AnonFunc {
   def sumInts (a: Int, b: Int) = sum(x => x, a, b)
   def sumCubes(a: Int, b: Int) = sum(x => x * x * x, a, b)
 
-}
-
-object AnonFunc {
-  def example: Unit = {
-    // println(sumInts(1, 5))
-    // println(sumCubes(1, 6))
-  }
 }
